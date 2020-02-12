c26350c80946fe87d3cc91ff9ab9ef4e7b3144b3
Agmar Torres <agmartorres@gmail.com>
2020-02-12 07:02:30 -0300

    start development

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('renders learn react link', () => {
+	test("render the title of counter", () => {
+		 const {getByText} = render(<App />)
+		 const linkElement = getByText( 'This is counter app')
+		 expect(linkElement).toBeInTheDocument();
+	})
+}) 
