79b1e46918f7666bad70daef686cb3502e516180
Agmar Torres <agmartorres@gmail.com>
2020-02-12 09:53:48 -0300

    understanding logic of jest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ counter, setcounter] = React.useState(0)
   return (
     <div className="App">
       <h1> This is counter app</h1>
+      <button id="increment-btn" onClick={()=> counter + 1}>Increment</button>
+      <div id="counter-value">{counter}</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({ adapter: new Adapter() });
 
 describe('renders learn react link', () => {
-	test("render the title of counter", () => {
+  /*
+  test("render the title of counter", () => {
 		 const {getByText} = render(<App />)
 		 const linkElement = getByText( 'This is counter app')
 		 expect(linkElement).toBeInTheDocument();
-	})
+  })
+  */
+  let wrapper;
+  beforeEach(()=> {
+    wrapper = shallow(<App />)    
+  })
+  
+  test("render a button with text of `increment`", () => {
+    wrapper = shallow(<App />)
+    expect( wrapper.find("#increment-btn").text()).toBe("Increment")
+  })
+
+  test("render h1 with text ",()=>{
+    expect(wrapper.find("h1").text()).toContain("This is counter app")
+  })
+
+  test("teste counter",()=>{
+    wrapper = shallow(<App/>)
+    expect(wrapper.find("#counter-value").text()).toBe("0")
+  })
+  
 }) 
